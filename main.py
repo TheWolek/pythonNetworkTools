e5e605c262303f386f1f4e9ce2ac6aa1209b719b
TheWolek <pawelm916@wp.pl>
2020-11-22 22:37:02 +0100

    createDump function; flags handling

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import json
+import sys
 
 from nics import getNics
 from diagnoze import diagnoze
 
 
 def main():
-    nics = getNics()
-    results = diagnoze()
-    toDump = {
-        "nics": nics,
-        "testResults": results
-    }
+    conductedTests = []
 
-    with open("output.json", "w") as f:
-        json.dump(toDump, f)
+    def getFlags():
+        args = sys.argv
+
+        if len(args) - 1 > 0:
+            args.pop(0)
+            return args
+        else:
+            return False
+
+    def createDump():
+        toDump = {
+            "nics": nics
+        }
+        if "diagnoze" in conductedTests:
+            toDump["testResults"] = results
+
+        with open("output.json", "w") as f:
+            json.dump(toDump, f)
+
+        return
+
+    flags = getFlags()
+    if flags:
+        if "-diag" in flags:
+            conductedTests.append("diagnoze")
+            results = diagnoze()
+
+    nics = getNics()
+    createDump()
 
 
 if __name__ == "__main__":
