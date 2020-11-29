50d56fd2657941a7174a6bcdd63047c3b4619f7c
TheWolek <pawelm916@wp.pl>
2020-11-29 19:37:52 +0100

    diagnoze.py cleanup

--- diagnoze.py
+++ diagnoze.py
@@ -1,9 +1,10 @@
+def diagnoze(nics):
+    nics = nics[0]
 
-
-def diagnoze():
     def testDNS():
         return
 
     def testGateway():
         return
-    return {'Gateway': '(5/5) OK', 'DNS': '(5/5) OK'}
+
+    return {'Gateway': testGateway()}
